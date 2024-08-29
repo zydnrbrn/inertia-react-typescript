4b47435e3077ad36bdb10112ddebe94659d30fa1
zydnrbrn <zidanreborn@gmail.com>
2024-08-29 17:08:24 +0700

    feat: Add Providers component for wrapping the App component with context providers

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,7 @@ import '../css/app.css';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import Providers from './providers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -11,12 +12,17 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
+        const appElement = (
+            <Providers>
+                <App {...props} />
+            </Providers>
+        );
         if (import.meta.env.DEV) {
-            createRoot(el).render(<App {...props} />);
+            createRoot(el).render(appElement);
             return
         }
 
-        hydrateRoot(el, <App {...props} />);
+        hydrateRoot(el, appElement);
     },
     progress: {
         color: '#4B5563',
