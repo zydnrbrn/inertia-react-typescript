2fed7fd94c4ca39f41e92aa0e1211a313ba5e4ec
zydnrbrn <zidanreborn@gmail.com>
2024-08-29 17:11:06 +0700

    refactor: Update profile edit page to use DashboardLayout and remove unused imports

--- resources/js/pages/profile/edit.tsx
+++ resources/js/pages/profile/edit.tsx
@@ -1,18 +1,15 @@
-import AuthenticatedLayout from '@/layouts/authenticated-layout';
 import DeleteUserForm from './partials/delete-user-form';
 import UpdatePasswordForm from './partials/update-password-form';
 import UpdateProfileInformationForm from './partials/update-profile-information';
-import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import { DashboardLayout } from '@/layouts/dashboard-layout';
 
 export default function Edit({ auth, mustVerifyEmail, status }: PageProps<{ mustVerifyEmail: boolean, status?: string }>) {
     return (
-        <AuthenticatedLayout
+        <DashboardLayout
+            title='Profile'
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
         >
-            <Head title="Profile" />
-
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                     <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
@@ -32,6 +29,6 @@ export default function Edit({ auth, mustVerifyEmail, status }: PageProps<{ must
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </DashboardLayout>
     );
 }
