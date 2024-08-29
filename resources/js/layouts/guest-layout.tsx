aa51da62b8e87134c20be01aabc87e6cf15165f4
zydnrbrn <zidanreborn@gmail.com>
2024-08-29 17:10:34 +0700

    refactor: Update guest layout to improve user experience

--- resources/js/layouts/guest-layout.tsx
+++ resources/js/layouts/guest-layout.tsx
@@ -4,16 +4,41 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <main className="w-full flex">
+            <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
+                <div className="relative z-10 w-full max-w-md">
+                    <img src="https://floatui.com/logo-dark.svg" width={150} />
+                    <div className=" mt-16 space-y-3">
+                        <h3 className="text-white text-3xl font-bold">Start growing your business quickly</h3>
+                        <p className="text-gray-300">
+                            Create an account and get access to all features for 30-days, No credit card required.
+                        </p>
+                        <div className="flex items-center -space-x-2 overflow-hidden">
+                            <img src="https://randomuser.me/api/portraits/women/79.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
+                            <img src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
+                            <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f" className="w-10 h-10 rounded-full border-2 border-white" />
+                            <img src="https://randomuser.me/api/portraits/men/86.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
+                            <img src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e" className="w-10 h-10 rounded-full border-2 border-white" />
+                            <p className="text-sm text-gray-400 font-medium translate-x-5">
+                                Join 5.000+ users
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="absolute inset-0 my-auto h-[500px]"
+                    style={{
+                        background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)", filter: "blur(118px)"
+                    }}
+                >
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+                </div>
             </div>
-        </div>
-    );
+            <div className="flex-1 flex items-center justify-center h-screen">
+                <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
+                    {children}
+                </div>
+            </div>
+        </main>
+    )
 }
