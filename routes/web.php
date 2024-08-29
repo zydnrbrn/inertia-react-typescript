9fbf4bcb3b3bda3803a29636de7bc94ea43ac7d8
zydnrbrn <zidanreborn@gmail.com>
2024-08-29 17:11:14 +0700

    refactor: Update web.php to remove unused code and improve performance

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
