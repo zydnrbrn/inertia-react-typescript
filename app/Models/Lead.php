5197562beb0a401a57b230e9fe595b3aaef9f6a9
zydnrbrn <zidanreborn@gmail.com>
2024-08-30 15:12:45 +0700

    feat: Add fillable fields to Lead model

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Lead extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'contact_id',
+        'status',
+        'score',
+        'source',
+        'description',
+    ];
 }
