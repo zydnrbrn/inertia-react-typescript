bd4136316488011531c76a73e7f1747cb382c1f4
zydnrbrn <zidanreborn@gmail.com>
2024-08-30 15:12:38 +0700

    feat: Add fillable fields to Deal model

--- app/Models/Deal.php
+++ app/Models/Deal.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Deal extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'lead_id',
+        'status',
+        'value',
+        'currency',
+        'description',
+    ];
 }
