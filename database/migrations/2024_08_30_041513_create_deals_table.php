79f89140f007bfefe5e69321f416c4f2bcf311bf
zydnrbrn <zidanreborn@gmail.com>
2024-08-30 14:24:55 +0700

    feat: Create migration for deals table

---
+++ database/migrations/2024_08_30_041513_create_deals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lead_id')->constrained('leads');
+            $table->string('name');
+            $table->decimal('amount', 10, 2);
+            $table->string('stage');
+            $table->date('expected_close_date');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deals');
+    }
+};
