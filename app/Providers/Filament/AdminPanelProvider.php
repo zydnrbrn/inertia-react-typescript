28b6f6ca1fed2760b53c6a0b0fd3bbb3b8c6a571
zydnrbrn <zidanreborn@gmail.com>
2024-11-05 21:30:58 +0700

    feat: enable collapsible sidebar on desktop for admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
+            ->sidebarCollapsibleOnDesktop()
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
