21ab58a671bdc58ebb4b761502a668999e2bad96
zydnrbrn <zidanreborn@gmail.com>
2024-11-05 21:29:33 +0700

    feat: add hasnayen/themes for theming

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,9 +50,13 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                \Hasnayeen\Themes\Http\Middleware\SetTheme::class
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                \Hasnayeen\Themes\ThemesPlugin::make()
             ]);
     }
 }
