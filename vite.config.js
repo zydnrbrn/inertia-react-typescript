f267fae68a0f5d634f8e6c3b7adbda85f8dcf623
zydnrbrn <zidanreborn@gmail.com>
2024-11-05 21:48:46 +0700

    feat: update Vite configuration to include additional input files and Livewire refresh paths

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: [
+                'resources/js/app.tsx',
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             ssr: 'resources/js/ssr.tsx',
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**'
+            ],
         }),
         react(),
     ],
